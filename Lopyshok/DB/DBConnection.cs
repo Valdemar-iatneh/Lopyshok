ee63bcec8c3c3bb326a137bb4996653d5d1c90fa
Valdemar-iatneh <avvakumov112003@gmail.com>
2023-02-11 10:09:17 +0300

    Add project files.

---
+++ Lopyshok/DB/DBConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lopyshok.DB
+{
+    public class DBConnection
+    {
+        public static LopyshokEntities connection = new LopyshokEntities();
+    }
+}
